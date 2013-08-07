6393f7c3eefe13aff9c9b50f9bdea0e3e7888a7a
flassa <flassa@flassa>
2013-08-07 17:25:31 +0200

    Primera prueba con Notificaciones Push

--- www/js/index.js
+++ www/js/index.js
@@ -68,53 +68,52 @@ var app = {
         }
 
     },
-    /*
-     // result contains any message sent from the plugin call
-     successHandler: function(result) {
-     alert('Callback Success! Result = ' + result);
-     },
-     errorHandler: function(error) {
-     alert(error);
-     },
-     onNotificationGCM: function(e) {
-     switch( e.event )
-     {
-     case 'registered':
-     if ( e.regid.length > 0 )
-     {
-     console.log("Regid " + e.regid);
-     //alert('registration id = '+e.regid);
-     }
-     break;
-     
-     case 'message':
-     // this is the actual push notification. its format depends on the data model from the push server
-     alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-     break;
-     
-     case 'error':
-     alert('GCM error = '+e.msg);
-     break;
-     
-     default:
-     alert('An unknown GCM event has occurred');
-     break;
-     }
-     },
-     onNotificationAPN: function(event) {
-     var pushNotification = window.plugins.pushNotification;
-     alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
-     
-     if (event.alert) {
-     navigator.notification.alert(event.alert);
-     }
-     if (event.badge) {
-     pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
-     }
-     if (event.sound) {
-     var snd = new Media(event.sound);
-     snd.play();
-     }
-     }
-     */
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Callback Success! Result = ' + result);
+    },
+    errorHandler: function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch (e.event)
+        {
+            case 'registered':
+                if (e.regid.length > 0)
+                {
+                    console.log("Regid " + e.regid);
+                    //alert('registration id = '+e.regid);
+                }
+                break;
+
+            case 'message':
+                // this is the actual push notification. its format depends on the data model from the push server
+                alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
+                break;
+
+            case 'error':
+                alert('GCM error = ' + e.msg);
+                break;
+
+            default:
+                alert('An unknown GCM event has occurred');
+                break;
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
+
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+    }
+
 };
